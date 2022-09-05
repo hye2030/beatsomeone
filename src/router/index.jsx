28c5977229cdefb588eaf4f560760d82fd59ac16
hyeri <xlsksl@naver.com>
2022-09-05 11:09:40 +0900

    import 수정

--- src/router/index.jsx
+++ src/router/index.jsx
@@ -2,8 +2,6 @@ import { useRoutes } from "react-router-dom";
 import SideMenu from "../layouts/side-menu/Main";
 import SimpleMenu from "../layouts/simple-menu/Main";
 import TopMenu from "../layouts/top-menu/Main";
-import Page1 from "../views/page-1/Main";
-import Page2 from "../views/page-2/Main";
 
 import BeatLayout from "../layouts/beatsomeone/Main"
 import BeatIndex from "../views/index"
