444e09cb837f851337cdc43a9047e032c43a0375
hyeri <xlsksl@naver.com>
2022-10-05 03:42:01 +0900

    피드 업로드

--- src/views/feed/feed_add.jsx
+++ src/views/feed/feed_add.jsx
@@ -1,26 +1,73 @@
+import { useState } from "react";
+import { useNavigate } from 'react-router-dom';
 import "@/assets/css/components/content_add.css";
 
-function add_daily(){
-    return (
-        <div className="add_content" style={{display: "block"}}>
+function Main() {
+    const navigate = useNavigate();
+
+    /**최상단 이미지 처리 및 미리보기 */
+    const [fileImage, setFileImage] = useState("");
+    const [files, setFiles] = useState("")
+    const saveFileImage = (e) => {
+        setFileImage(URL.createObjectURL(e.target.files[0]));
+        setFiles(e.target.files);
+    };
+    const deleteFileImage = () => {
+        URL.revokeObjectURL(fileImage);
+        setFileImage("");
+        setFiles("");
+    };
+
+    /**일상 추가용 */
+    const [addDaily, setAddDaily] = useState([]);
+    const [nextId, setNextId] = useState(5);
+
+    const handleClick = () => {
+        const newList = addDaily.concat({
+            id: nextId
+        });
+        setNextId(nextId+1);
+        setAddDaily(newList);
+    }
+    const handleDelete = id => {
+        const newList = addDaily.filter(daily => daily.id !== id);
+        setAddDaily(newList);
+    };
+
+    const handleImage = (e, index) => {
+        let cur_file = e.target.files[0];
+        const filesInArr = Array.from(e.target.files);
+        const previewArr = [window.URL.createObjectURL(cur_file)];
+        console.log(filesInArr);
+        console.log(previewArr);
+    }
+
+    const submitClick = async (e) => {
+        e.preventDefault();
+        e.persist();
+
+        console.log($("input[name=daily_files]").length);
+    }
+
+    const dailyList = addDaily.map((daily, index) => 
+        <div className="add_content" style={{display: "block"}} key={daily.id}>
             <div className="img_add_line">
-            <input type="text" placeholder="이미지 및 영상선택" className="add_input" />
-            <label htmlFor="file" className="add_btn"> 추가 </label>
-            <input type="file" id="file" />
+                <input type="text" placeholder="이미지 및 영상선택" className="add_input" readOnly />
+                <label htmlFor="file" className="add_btn"> 추가 </label>
+                <input type="file" id="file" accept="image/*" name="daily_files" onChange={(e) => {handleImage(e, index);}} />
             </div>
             <div className="textarea_wrap">
-            <textarea placeholder="내용 입력 (5,000 글자)" className="content_area"></textarea>
-            <span className="list_delete_icon">
-                <img src="/src/assets/images/icon/icon_delete_cover.svg" alt="" />
-            </span>
+                <textarea placeholder="내용 입력 (5,000 글자)" className="content_area"></textarea>
+                <span className="list_delete_icon" onClick={() => handleDelete(daily.id)}>
+                    <img src="/src/assets/images/icon/icon_delete_cover.svg" alt="" />
+                </span>
             </div>
         </div>
-    )
-}
+    );
 
-function Main() {
     return (
         <>
+        <form onSubmit={(e) => submitClick(e)}>
         <div id="wrap_content" className="content_add_wrap">
             <div className="wrap_inner">
                 <section className="feed_add_section">
@@ -43,17 +90,19 @@ function Main() {
                     {/* 이미지 선택됐을때 gray 클래스를 추가해주시면 됩니다! */}
                     {/* <input type="text" value="file.jpg" className="add_input gray" readonly /> */}
                     <label htmlFor="image" className="add_input">이미지 및 영상선택</label>
-                    <input type="file" id="image" />
+                    <input type="file" id="image" accept="image/*" onChange={saveFileImage}/>
                     </div>
                     {/* <!-- 이미지 추가 했을 때 --> */}
+                    {fileImage && (
                     <div className="add_file_box">
-                    <div className="cover_img">
-                        <img src="/src/assets/images/dummy/cover_img_03.jpg" alt="" />
+                        <div className="cover_img">
+                            <img src={fileImage} alt="" />
+                        </div>
+                        <span className="close_icon" onClick={() => deleteFileImage()}>
+                            <img src="/src/assets/images/icon/icon_close_white.svg" alt="" />
+                        </span>
                     </div>
-                    <span className="close_icon">
-                        <img src="/src/assets/images/icon/icon_close_white.svg" alt="" />
-                    </span>
-                    </div>
+                    )}
                     <textarea placeholder="내용 입력 (5,000 글자)" className="content_area"></textarea>
                     {/* <!-- 영상 추가 했을 때 --> */}
                     {/* <div className="add_file_box">
@@ -72,26 +121,15 @@ function Main() {
                     </div>
                     <textarea placeholder="내용 입력 (5,000 글자)" className="content_area"></textarea> */}
                 </div>
-                {/* <!-- 일상 선택했을 경우 추가되는 컨텐츠 --> */}
-                <div className="add_content" style={{display: "block"}}>
-                    <div className="img_add_line">
-                    <input type="text" placeholder="이미지 및 영상선택" className="add_input" />
-                    <label htmlFor="file" className="add_btn"> 추가 </label>
-                    <input type="file" id="file" />
-                    </div>
-                    <div className="textarea_wrap">
-                    <textarea placeholder="내용 입력 (5,000 글자)" className="content_area"></textarea>
-                    <span className="list_delete_icon">
-                        <img src="/src/assets/images/icon/icon_delete_cover.svg" alt="" />
-                    </span>
-                    </div>
-                </div>
+                
+                {/*일상 선택했을 경우 추가되는 컨텐츠*/}
+                {dailyList}
 
                 <div className="btn_wrap">
                     <p className="gray_text">
                     + 선택 시 내용과 이미지 및 영상 등록 영역이 추가됩니다.
                     </p>
-                    <button type="button" className="plus_btn">
+                    <button type="button" className="plus_btn" onClick={handleClick}>
                     <span>
                         <img src="/src/assets/images/icon/icon_plus_gray.svg" alt="" />
                     </span>
@@ -99,12 +137,13 @@ function Main() {
                 </div>
 
                 <div className="button_wrap">
-                    <button className="basic_btn_red_border cancel_btn">취소</button>
-                    <button className="basic_btn_red confirm_btn">등록</button>
+                    <button className="basic_btn_red_border cancel_btn" onClick={() => {navigate("/feed/feed_list")}}>취소</button>
+                    <button className="basic_btn_red confirm_btn" type="submit">등록</button>
                 </div>
                 </section>
             </div>
         </div>
+        </form>
         </>
     );
 }
