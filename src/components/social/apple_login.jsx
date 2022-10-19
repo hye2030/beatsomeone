092f1e527beca07ca4253bf2b8f00b85a656e0a7
hyeri <xlsksl@naver.com>
2022-10-19 22:53:08 +0900

    애플 이메일 중복 수정

--- src/components/social/apple_login.jsx
+++ src/components/social/apple_login.jsx
@@ -6,6 +6,7 @@ import { useDispatch, useSelector } from 'react-redux';
 import {loginUser} from '@/stores/userSlice';
 import { useRecoilState, useSetRecoilState, useRecoilValue } from 'recoil';
 import { isModal } from "../../components/header/recoil";
+import { LoginSocialApple } from 'reactjs-social-login';
 
 const AppleLogin = (e) => {
     const appleRef = useRef();
@@ -13,25 +14,113 @@ const AppleLogin = (e) => {
     const dispatch = useDispatch();
     const currentType = useRecoilValue(isModal);
 
-    var array = new Uint32Array(1);
-    window.crypto.getRandomValues(array);
+    // var array = new Uint32Array(1);
+    // window.crypto.getRandomValues(array);
 
-    AppleID.auth.init({
-        clientId : 'com.beatsomeone.codeidea.io.beatsomeone',
-        scope : 'email',
-        redirectURI: 'https://beatsomeone-aws.prefinc.kr',
-        state : toString(array[0]),
-        usePopup: true
-    });
+    // AppleID.auth.init({
+    //     clientId : 'com.beatsomeone.codeidea.io.beatsomeone',
+    //     scope : 'email',
+    //     redirectURI: 'https://beatsomeone.codeidea.io',
+    //     state : toString(array[0]),
+    //     usePopup: true
+    // });
 
-    useEffect(() => {
-    document.addEventListener('AppleIDSignInOnSuccess', (data) => {
-        const credential = jwt_decode(data.detail.authorization.id_token);
+    // useEffect(() => {
+    // document.addEventListener('AppleIDSignInOnSuccess', (data) => {
+        // const credential = jwt_decode(data.detail.authorization.id_token);
+
+        // localStorage.setItem("sign_id", credential.email);
+        // localStorage.setItem("sns", "apple");
+        // localStorage.setItem("snsKey", credential.sub);
+        // axios.get(import.meta.env.VITE_REACT_APP_API_URL +"/api/v1/member/joinCheck", {
+        //     params: {
+        //         emailId: credential.email,
+        //         sns : "apple",
+        //         snsKey: credential.sub
+        //     }
+        // })
+        // .then(function (response) {
+        //     if(response.data.code == 0){
+        //         if(response.data.response == 0){
+        //             // $('.route_modal.signIn').fadeOut(200);
+        //             // $('body').removeClass('scrollOff').off('scroll touchmove mousewheel');
+        //             $("#noSnsModal").fadeIn(200);
+        //             // navigate('/signinup/sign_up');
+        //         }else if(response.data.response == 1){
+        //             $('.route_modal.signIn').fadeOut(200);
+        //             $('body').removeClass('scrollOff').off('scroll touchmove mousewheel');
+        //             navigate('/signinup/integrated_signup_guide');
+        //         }else if(response.data.response == 3){
+        //             $('.route_modal.signIn').fadeOut(200);
+        //             $('body').removeClass('scrollOff').off('scroll touchmove mousewheel');
+
+        //             if(currentType == "join"){
+        //                 $("#localSNSImg").html('<div className="icon_box"><img src="/assets/images/icon/signUp_apple.svg"alt="" /></div>');
+        //                 $("#localSNSId").text("SNS 가입 ("+credential.email+")");
+        //                 $("#alreadyJoinModal").fadeIn(200);
+        //                 return false;
+        //             }
+
+        //             axios.put(import.meta.env.VITE_REACT_APP_API_URL +"/api/v1/member/login", {
+        //                 sns: "apple",
+        //                 snsKey: credential.sub
+        //             })
+        //             .then(function (responseLogin) {
+        //                 if(responseLogin.data.code == "0"){
+        //                     localStorage.setItem("emailId", responseLogin.data.response.email);
+        //                     localStorage.setItem("is_login", responseLogin.data._token);
+        //                     localStorage.setItem("last_login", "apple");
+        //                     localStorage.setItem("emailIdx", responseLogin.data.response.idx);
+        //                     localStorage.setItem("nickname", responseLogin.data.response.nickName);
+
+        //                     dispatch(loginUser({
+        //                         "response": {
+        //                             "nickName": responseLogin.data.response.nickName,
+        //                             "email": credential.sub,
+        //                             "idx": responseLogin.data.response.idx
+        //                         }
+        //                     }));
+
+        //                     $('.signIn_modal').fadeOut(200);
+        //                     $('body').removeClass('scrollOff').off('scroll touchmove mousewheel');
+        //                     navigate("/");
+        //                 }
+        //             });
+        //         }else if(response.data.response == 4){
+        //             $('.route_modal.signIn').fadeOut(200);
+        //             $('body').removeClass('scrollOff').off('scroll touchmove mousewheel');
+        //             alert("신규회원으로 가입한 아이디로 로그인해주세요.");
+        //             navigate("/");
+        //         }
+        //     }else{
+        //         navigate("/");
+        //     }
+        // })
+    // });
+    // }, [])
+
+    // //애플로 로그인 실패 시.
+    // document.addEventListener('AppleIDSignInOnFailure', (error) => {
+    //     navigate("/");
+    // });
+
+    // const handleAppleLogin = () => {
+	// 	appleRef.current.children[0].click();
+	// }
+
+    const AppleIDSuccess = (data) => {
+        const credential = jwt_decode(data.data.authorization.id_token);
 
         localStorage.setItem("sign_id", credential.email);
         localStorage.setItem("sns", "apple");
         localStorage.setItem("snsKey", credential.sub);
-        axios.get(import.meta.env.VITE_REACT_APP_API_URL +"/api/v1/member/joinCheck", {
+        
+        let apiUrl = "/api/v1/member/joinCheck";
+        if(currentType == "join"){
+            apiUrl = "/api/v1/member/joinEmailCheck";
+        }
+
+        axios.get(import.meta.env.VITE_REACT_APP_API_URL + apiUrl, {
             params: {
                 emailId: credential.email,
                 sns : "apple",
@@ -53,12 +142,12 @@ const AppleLogin = (e) => {
                     $('.route_modal.signIn').fadeOut(200);
                     $('body').removeClass('scrollOff').off('scroll touchmove mousewheel');
 
-                    // if(currentType == "join"){
-                    //     $("#localSNSImg").html('<div className="icon_box"><img src="/assets/images/icon/signUp_apple.svg"alt="" /></div>');
-                    //     $("#localSNSId").text("SNS 가입 ("+credential.email+")");
-                    //     $("#alreadyJoinModal").fadeIn(200);
-                    //     return false;
-                    // }
+                    if(currentType == "join"){
+                        $("#localSNSImg").html('<div className="icon_box"><img src="/assets/images/icon/signUp_apple.svg"alt="" /></div>');
+                        $("#localSNSId").text("SNS 가입 ("+credential.email+")");
+                        $("#alreadyJoinModal").fadeIn(200);
+                        return false;
+                    }
 
                     axios.put(import.meta.env.VITE_REACT_APP_API_URL +"/api/v1/member/login", {
                         sns: "apple",
@@ -95,25 +184,25 @@ const AppleLogin = (e) => {
                 navigate("/");
             }
         })
-    });
-    }, [])
-
-    //애플로 로그인 실패 시.
-    document.addEventListener('AppleIDSignInOnFailure', (error) => {
-        navigate("/");
-    });
-
-    const handleAppleLogin = () => {
-		appleRef.current.children[0].click();
-	}
+    }
 
     return (
         <>
-            <div ref={appleRef} id="appleid-signin" data-color="black" data-border="true" data-type="sign in" style={{display:"none"}}></div>
+            {/* <div ref={appleRef} id="appleid-signin" data-color="black" data-border="true" data-type="sign in" style={{display:"none"}}></div> */}
             {/* <button type="button" className="signIn_btn apple" onClick={handleAppleLogin}> */}
-            <button type="button" className={e.value == "new"? "signIn_btn apple newly" : "signIn_btn apple"} onClick={handleAppleLogin}>
+            {/* <button type="button" className={e.value == "new"? "signIn_btn apple newly" : "signIn_btn apple"} onClick={handleAppleLogin}>
                 Continue with Apple
-            </button>
+            </button> */}
+            <LoginSocialApple
+                client_id= "com.beatsomeone.codeidea.io.beatsomeone"
+                scope={'name email'}
+                redirect_uri='https://beatsomeone-aws.prefinc.kr'
+                onResolve={(data) => {AppleIDSuccess(data)}}
+                onReject={err => {
+                    console.log(err);
+                }}
+                ><button type="button" style={{marginBottom:"10px"}} className={e.value == "new"? "signIn_btn apple newly" : "signIn_btn apple"}>
+                Continue with Apple</button></LoginSocialApple>
         </>
     )
 }
